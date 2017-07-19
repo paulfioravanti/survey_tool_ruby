deb3d41b21249e3101bf918354485f5dd1efc8a9
Paul Fioravanti <paul.fioravanti@gmail.com>
2017-07-19 11:25:11 +1000

    Configure Simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "simplecov"
 require "survey_tool"
 
 require "minitest/autorun"
+require "minitest/mock"
 require "minitest/reporters"
 Minitest::Reporters.use!
+require "pry-byebug"

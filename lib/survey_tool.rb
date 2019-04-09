7740c3d40087a2abc49814e602618770289ecae8
Paul Fioravanti <paul.fioravanti@gmail.com>
2019-04-09 12:45:38 +0930

    Yard doc the def_delegator rather than the module_function

--- lib/survey_tool.rb
+++ lib/survey_tool.rb
@@ -9,16 +9,12 @@ Dir["#{__dir__}/survey_tool/**/*.rb"].each { |file| require file }
 module SurveyTool
   extend Forwardable
 
-  # Delegates directly Application.start
-  def_delegator Application, :start
-
   # @!method start
   #   Entry point for running the survey tool.
   #
   #   @return [nil]
   #   @see Application.start
-  # False positive: https://github.com/rubocop-hq/rubocop/issues/5953
-  # rubocop:disable Style/AccessModifierDeclarations
+  def_delegator Application, :start
+
   module_function :start
-  # rubocop:enable Style/AccessModifierDeclarations
 end
